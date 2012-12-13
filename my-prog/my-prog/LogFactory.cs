3192867432064645091c63bcb995bc43e2887305
richard <richard.delorenzi@gmail.com>
2012-12-13 10:31:55 +0000

    refactor: move logging classes to there own file.

---
+++ my-prog/my-prog/LogFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace my_prog
+{
+    sealed class LogFactory
+    {
+        private static Logger _theLogger = null;
+        public Logger theLogger()
+        {
+            //evaluate once (per class/system)
+            if (_theLogger == null)
+            {
+                _theLogger = new Logger();
+            }
+            return _theLogger;
+        }
+    }
+}
---
+++ my-prog/my-prog/Logger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace my_prog
+{
+    sealed class Logger : IDisposable
+    {
+        private bool is_first;
+
+        public Logger()
+        {
+            Write("[");
+            is_first = true;
+        }
+
+        private void Write(string message)
+        {
+            System.Console.Write(message);
+        }
+
+        public void log(object o)
+        {
+            Write(
+                (is_first ? "" : ",") +
+                Newtonsoft.Json.JsonConvert.SerializeObject(o)
+            );
+            is_first = false;
+        }
+
+        public void log(string s, object o)
+        {
+            log(new Dictionary<string, object>() { { s, o } });
+        }
+
+        public void logError(object o)
+        {
+            log("error", o);
+        }
+
+        public void logException(object o)
+        {
+            log("exception", o);
+        }
+
+        public void logEvent(string s, object o)
+        {
+            log("event:" + s, o);
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Write("]");
+        }
+
+        #endregion
+
+        ~Logger()
+        {
+            Dispose();
+        }
+    }
+}
