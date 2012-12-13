102e9a15a996c25b283369bd9ce2aa656b2b6678
richard <richard.delorenzi@gmail.com>
2012-12-13 10:01:23 +0000

    refactor: to make more portable fixed some bugs

--- my-prog/my-prog/Program.cs
+++ my-prog/my-prog/Program.cs
@@ -8,20 +8,45 @@ namespace my_prog
     sealed class Program {
         private readonly Logger _logger = new LogFactory().theLogger();
 
+        private Dictionary<string, string> lineToDict(string line)
+        {
+            var options = new Dictionary<string, string>(); ;
+            try
+            {
+                options = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(line);
+                options = (options == null)?new Dictionary<string,string>():options;
+            }
+            catch (Newtonsoft.Json.JsonReaderException)
+            {
+                var errorMessage=new Dictionary<string,string>();
+                errorMessage.Add("bad input", line);
+                _logger.logError(errorMessage);         
+            }
+
+            return options;
+        }
+
+        private Dictionary<string, string> ReadLine()
+        {
+            var line = System.Console.ReadLine();
+            if (line == null )
+            {
+                return null;
+            }
+            else
+            {
+                return lineToDict(line);
+            }
+        }
+
         public void start() {
             bool is_finished=false;
             while (! is_finished) {
-                var line = System.Console.ReadLine();
-                if (line == null || line == "") {
-                    is_finished = true;
-                } else {
-                    try {
-                        Dictionary<string, string> options = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(line);
-                        new Application(options).Run();
-                    }
-                    catch (Newtonsoft.Json.JsonSerializationException) {
-                        _logger.logError("input error");
-                    }
+                Dictionary<string, string> options = ReadLine();
+                is_finished = (options == null);
+                if (!is_finished && options.Count > 0)
+                {
+                    new Application(options).Run();
                 }
             }
         }
